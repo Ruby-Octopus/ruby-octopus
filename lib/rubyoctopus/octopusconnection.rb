ccd31a4efe7bb54eb8c0c491dd6e22b884edaa2e
Jeff Merckens <jeff.merckens@tylertech.com>
2022-01-21 11:25:40 +0000

    Start basic OctopusConnection to wrap Faraday connection

---
+++ lib/rubyoctopus/octopusconnection.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "faraday"
+require "faraday_middleware"
+
+module RubyOctopus
+  # Base class to use to perform actions against an Octopus instance.
+  class OctopusConnection
+    # Users might want to check which domain an instance is targeting.
+    attr_reader :url
+
+    # No need to expose the connection object.
+    @conn = nil
+
+    def initialize(url, api_key)
+      @url = url
+      @conn = Faraday.new(
+        url: url,
+        headers: { "X-Octopus-ApiKey": api_key }
+      ) do |faraday|
+        faraday.adapter Faraday.default_adapter
+        faraday.response :json
+      end
+    end
+
+    def get(resource_type)
+      @conn.get(resource_type)
+    end
+  end
+end

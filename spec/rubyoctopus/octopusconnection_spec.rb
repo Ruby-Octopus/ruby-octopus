520a393a0f0e8750d47c7efc1483097a67fb6603
M Emery <thepolytheist@users.noreply.github.com>
2022-01-28 01:49:47 +0000

    test: add OctopusConnection and OctopusClient coverage
    test: add client list and paginate coverage

--- spec/rubyoctopus/octopusconnection_spec.rb
+++ spec/rubyoctopus/octopusconnection_spec.rb
@@ -3,11 +3,30 @@
 TEST_URL = "http://www.example.com"
 TEST_API_KEY = "API-FAKEAPIKEY"
 TEST_PATH = "Environments"
+PARAM_TEST_PATH = "param-test"
 
 RSpec.describe RubyOctopus::OctopusConnection do
   before(:each) do
     # Set up Faraday mock
-    @faraday = double("Faraday")
+    @faraday = Faraday.new do |builder|
+      builder.adapter :test do |stub|
+        stub.get("/Environments") do |_env|
+          [
+            200,
+            { "Content-Type": "text/plain" },
+            "shrimp"
+          ]
+        end
+
+        stub.get("/param-test?key=value") do |_env|
+          [
+            200,
+            {},
+            "Param test response body"
+          ]
+        end
+      end
+    end
     allow(@faraday).to receive(:adapter).with(Faraday.default_adapter)
     allow(@faraday).to receive(:response).with(:json)
     allow(Faraday).to receive(:new).and_yield(@faraday).and_return(@faraday)
@@ -17,17 +36,18 @@ RSpec.describe RubyOctopus::OctopusConnection do
   end
 
   it "has the correct URL" do
-    expect(@conn.url).to be TEST_URL
+    expect(@conn.url).to eq(TEST_URL)
   end
 
   it "gets the correct path" do
-    # Set up response mock
-    test_response = double("Response")
-    allow(@faraday).to receive(:get).with(TEST_PATH).and_return(test_response)
-
     response = @conn.get(TEST_PATH)
 
     # Assert response matches mock
-    expect(response).to be test_response
+    expect(response.status).to be 200
+  end
+
+  it "gets the correct params" do
+    response = @conn.get(PARAM_TEST_PATH, { "key" => "value" })
+    expect(response.body).to be "Param test response body"
   end
 end
